0a339fe87d2846dcd88436bc444ff648022fefc1
dl10yr <50223759+dl10yr@users.noreply.github.com>
2020-01-06 08:04:58 +0900

    Update Cards.js

--- src/Cards.js
+++ src/Cards.js
@@ -8,6 +8,12 @@ class SwipeCards extends Component {
     this.state = {
       like: false,
       dislike: false,
+      nolike: false,
+      nolike2: false,
+      alertLeft: false,
+      alertRight: false,
+      alertTop: false,
+      alertBottom: false,
       index: 0,
       containerSize: { x: 0, y: 0 }
     }
@@ -17,16 +23,23 @@ class SwipeCards extends Component {
     const { children, onEnd } = this.props
 
     if (children.length === (this.state.index + 1) && onEnd) onEnd()
+    
+    setTimeout(() => this.setState({ [`alert${side}`]: false }), 300)
 
     this.setState({
       like: false,
       dislike: false,
-      index: this.state.index + 1
+      nolike: false,
+      nolike2: false,
+    })
+    this.setState({
+      index: this.state.index + 1,
+      [`alert${side}`]: true
     })
   }
 
   like() {
-    if (!this.state.like && !this.state.dislike) {
+    if (!this.state.like && !this.state.dislike && !this.state.nolike && !this.state.nolike2) {
       this.setState({
         like: true
       });
@@ -34,16 +47,32 @@ class SwipeCards extends Component {
   }
 
   dislike() {
-    if (!this.state.like && !this.state.dislike) {
+    if (!this.state.like && !this.state.dislike && !this.state.nolike && !this.state.nolike2) {
       this.setState({
         dislike: true
       });
     }
   }
+  
+  nolike() {
+    if (!this.state.like && !this.state.dislike && !this.state.nolike && !this.state.nolike2) {
+      this.setState({
+        nolike: true
+      });
+    }
+  }
+  
+  nolike2() {
+    if (!this.state.like && !this.state.dislike && !this.state.nolike && !this.state.nolike2) {
+      this.setState({
+        nolike2: true
+      });
+    }
+  }  
 
   render() {
-    const { like, dislike, index } = this.state
-    const { children, className, likeOverlay, dislikeOverlay } = this.props
+    const { like, dislike, index, nolike, nolike2 } = this.state
+    const { children, className, likeOverlay, dislikeOverlay, nolikeOverlay, nolike2Overlay } = this.props
 
     const _cards = children.reduce((memo, c, i) => {
       if (index > i) return memo
@@ -56,13 +85,20 @@ class SwipeCards extends Component {
         like,
         dislike,
         likeOverlay,
-        dislikeOverlay
+        dislikeOverlay,
+        nolikeOverlay.
+        nolike2Overlay
       }
       return [cloneElement(c, props), ...memo]
     }, [])
 
     return (
       <div className={className}>
+        {DIRECTIONS.map(d => 
+          <div key={d} className={`${this.state[`alert${d}`] ? 'alert-visible': ''} alert-${d.toLowerCase()} alert`}>
+            {this.props[`alert${d}`]}
+          </div>
+        )}
         <div id='cards'>
           {_cards}
         </div>
